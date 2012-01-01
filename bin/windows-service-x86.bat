df18a2c3e006b2fc29137707486a7b38f570354d
Phil Ostler <philostler@gmail.com>
2012-01-01 04:24:29 +0000

    Initial commit

---
+++ bin/windows-service-x86.bat
@@ -0,0 +1,7 @@
+@echo off
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+sc create Redis displayname= Redis binpath= "\"%cd%\x86\redis-service.exe\" \"%cd%\redis.conf\"" start= auto
+sc description Redis "Provides advanced key-value data storage (32-bit). It is often referred to as a data structure server since keys can contain strings, hashes, lists, sets and sorted sets."
+sc failure Redis actions= restart/10000 reset= 60
